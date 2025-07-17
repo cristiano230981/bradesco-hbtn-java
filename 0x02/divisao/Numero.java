48bbf2b0cf7ab0fad50683e1081c7b2823206bfd
Cristiano <cristiano@vbsolutions.com.br>
2025-07-17 13:35:35 -0300

    bradesco-hbtn-java - Divisão inteira

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -4,7 +4,7 @@ public class Numero {
         try {
             resultado = a / b;
         } catch (Exception e) {
-            System.out.printf("Nao foi possivel dividir %d por %d\n", a, b);
+            System.out.printf("Nao eh possivel dividir por zero\n");
         } finally {
             System.out.printf("%d / %d = %d\n", a, b, resultado);
         }
