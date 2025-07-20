2b25779aaee07d488929c92276a1144883bd47dc
Cristiano <cristiano@vbsolutions.com.br>
2025-07-20 09:04:03 -0300

    bradesco-hbtn-java - Complexidade O(n) – Busca Linear

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -21,16 +21,19 @@ public class PessoasArray {
 
     public void buscaLinear(String nome) {
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
-        boolean nomeEncontrado = false;
+
+        boolean encontrado = false;
+
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
-            if (nomes[i] == nome) {
+            if (nomes[i].equals(nome)) {
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
-                nomeEncontrado = true;
+                encontrado = true;
             }
         }
 
-        if(nomeEncontrado == false)
+        if (!encontrado) {
             throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
     }
 }
