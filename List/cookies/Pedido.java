139aff601569dc9baf530ce1c72c3ae3be56a2c7
Cristiano <cristiano@vbsolutions.com.br>
2025-07-21 07:53:44 -0300

    bradesco-hbtn-java - Cookies

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Pedido {
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedido) {
+        cookies.add(pedido);
+    }
+
+    public int obterTotalCaixas() {
+        int total = 0;
+        for (PedidoCookie cookie : cookies) {
+            total += cookie.getQuantidadeCaixas();
+        }
+        return total;
+    }
+
+    public int removerSabor(String sabor) {
+        int removidos = 0;
+        Iterator<PedidoCookie> iterator = cookies.iterator();
+
+        while (iterator.hasNext()) {
+            PedidoCookie cookie = iterator.next();
+            if (cookie.getSabor().equals(sabor)) {
+                removidos += cookie.getQuantidadeCaixas();
+                iterator.remove();
+            }
+        }
+
+        return removidos;
+    }
+}
