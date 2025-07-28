b1d7f73da9128e24726b1b858e44a8a74fa18850
Cristiano <cristiano@vbsolutions.com.br>
2025-07-28 17:23:22 -0300

    bradesco-hbtn-java -  Eletronicos com 15% de desconto

---
+++ Streams_Collectors_Optional/desconto_eletronicos/Produto.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(new Locale("pt", "BR"),"[%d] %s %s R$ %.2f", codigo, nome, categoria, preco);
+    }
+
+
+    public double getPreco() {
+        return preco;
+    }
+
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
